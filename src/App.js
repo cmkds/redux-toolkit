e528440b86b8aa753ada776ae5351eabbb038b5f
cmkds <107685502+cmkds@users.noreply.github.com>
2023-03-03 10:18:15 +0900

    redux-toolkit 생활코딩

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createStore } from "redux";
+import { Provider, useSelector, useDispatch } from "react-redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { value: 0 },
+  reducers: {
+    up: (state, action) => {
+      state.value = state.value + action.step;
+    },
+  },
+});
+
+const store = configureStore({
+  // 카운터에 대한 리듀서다.
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
+
+// function reducer(state, action) {
+//   if (action.type === "up") {
+//     return { ...state, value: state.value + action.step };
+//   }
+
+//   return state;
+// }
+
+// const initialState = { value: 1 };
+// const store = createStore(reducer, initialState);
+
+function Counter() {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => {
+    console.log(state);
+    return state.counter.value;
+  });
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch({ type: "up", step: 2 });
+        }}
+      >
+        +
+      </button>
+      {count}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Counter></Counter>
+      </div>
+    </Provider>
   );
 }
 
